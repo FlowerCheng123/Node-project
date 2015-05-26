c0870bfcabd5e70cbd563a8ee1a0030aacb57270
FlowerCheng123 <cheng_cong123@163.com>
2015-05-26 16:32:18 +0800

    init project1.0

--- myapp/routes/index.js
+++ myapp/routes/index.js
@@ -1,10 +1,12 @@
 var express = require('express');
 var router = express.Router();
+var path = require('path');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   // res.render('index', { title: 'Express' });
-  res.sendFile('pier/index.html');
+  res.sendFile(path.join(__dirname, '../pier', 'index.html'));
+  // res.sendFile('pier/index.html');
 });
 
 module.exports = router;
